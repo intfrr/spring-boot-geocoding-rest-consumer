670574e0fec10d42d29e2ce1c4f181d836cb2c3d
Punane Madu <kaldt.slange@gmail.com>
2021-02-07 19:31:17 -0600

    2102071931 last changes for submodules

--- src/main/java/org/nil/ApiKeyConfig.java
+++ src/main/java/org/nil/ApiKeyConfig.java
@@ -1,20 +1,38 @@
 package org.nil;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
-@Configuration
-@PropertySource("classpath:apikey.properties")
-public class ApiKeyConfig {
-	
-	@Value("${apiKey}")
-	private String apiKey;
-	
-	@Bean
-	public static PropertySourcesPlaceholderConfigurer getPropertyConfig() {
-		return new PropertySourcesPlaceholderConfigurer();
-	}	
+//@Configuration
+//@PropertySource("classpath:apikey.properties")
+//@PropertySource("classpath:application.properties")
+public class ApiKeyConfig /*implements EnvironmentAware */{
+//
+////	@Value("${apiKey}")
+//	@Value("${google.map.api.key}")
+////  private String endPoint;
+//	private String apiKey;
+//
+//	private Environment environment;
+//
+//	@Override
+//	public void setEnvironment(final Environment environment) {
+//		this.environment = environment;
+//	}
+//
+//	@Bean
+//	public static PropertySourcesPlaceholderConfigurer getPropertyConfig() {
+//		return new PropertySourcesPlaceholderConfigurer();
+//	}
+//
+////	public void myMethod() {
+////		final String apiKeyValue = environment.getProperty("google.map.api.key");
+////		// ...
+////	}
+//
 }
--- src/main/java/org/nil/GeoCodingApiController.java
+++ src/main/java/org/nil/GeoCodingApiController.java
@@ -2,8 +2,7 @@ package org.nil;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,18 +14,27 @@ public class GeoCodingApiController {
 
 	private static final Logger log = LoggerFactory.getLogger(GeoCodingApiController.class);
 	private static final String GEOCODING_URI = "https://maps.googleapis.com/maps/api/geocode/json";
-	
-	@Autowired
-	private Environment env;
+
+//	@Autowired
+//	private Environment env;
+
+	@Value("${google.map.api.key}")
+	private String apiKey;
 
 	@RequestMapping("/getGeoCoding")
-	public GeoCoding getGeoCodingForLoc(@RequestParam(value = "address", defaultValue="silicon+valley") String address) {
+	public GeoCoding getGeoCodingForLoc(
+			@RequestParam(value = "address", defaultValue = "silicon+valley") String address) {
 		RestTemplate restTemplate = new RestTemplate();
+
+//		System.out.println("apiKey: " + env.getProperty("apiKey"));
+		System.out.println("apiKey: " + apiKey);
+
 		UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(GEOCODING_URI).queryParam("address", address)
-				.queryParam("key", env.getProperty("apiKey"));
-			
+//				.queryParam("key", env.getProperty("apiKey"));
+				.queryParam("key", apiKey);
+
 		log.info("Calling geocoding api with: " + builder.toUriString());
-		
+
 		GeoCoding geoCoding = restTemplate.getForObject(builder.toUriString(), GeoCoding.class);
 		log.info(geoCoding.toString());
 
